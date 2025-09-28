58025f09d0efeba41afceaf963ced10c2319110b
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-09-28 19:20:03 +0600

    feat(nodemailer): add nodemailer configuration for email transport

---
+++ server/config/nodemailer.js
@@ -0,0 +1,11 @@
+import e from "express";
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+export default transporter;
