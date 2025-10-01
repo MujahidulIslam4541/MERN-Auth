021486a0e297f8bf380821d36cbdaa01151936d9
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-01 18:22:15 +0600

    feat create homepage,loginPage,passwordeset,emalverifiation

---
+++ client/src/pages/EmailVarification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmailVerification = () => {
+    return (
+        <div>
+            <h2>email verification</h2>
+        </div>
+    )
+}
+
+export default EmailVerification
