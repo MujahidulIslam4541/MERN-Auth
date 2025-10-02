1345c3c0293f5342a928ac0f1c7809de785a2a9a
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-02 22:08:43 +0600

    feat passwordReset onsubmitEmail functonality implement

--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -1,8 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { assets } from '../../public/assets'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { FaEye, FaEyeSlash } from 'react-icons/fa'
+import { AppContent } from '../context/AppContent'
+import toast from 'react-hot-toast'
 
 const PasswordReset = () => {
   const navigate = useNavigate()
@@ -10,8 +12,14 @@ const PasswordReset = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
+
+
+  const [isEmailSend, setIsEmailSend] = useState('')
+  const [otp, setOtp] = useState(0)
+  const [isOtpSubmitted, setIsOtpSubmitted] = useState(0)
+
   axios.defaults.withCredentials = true;
-  // const { backendUrl, isLoggedIn, getUserData, userData } = useContext(AppContent)
+  const { backendUrl } = useContext(AppContent)
 
   const handleInput = (e, index) => {
     if (e.target.value.length > 0 && index < inputRef.current.length - 1) {
@@ -35,6 +43,24 @@ const PasswordReset = () => {
   }
 
 
+  // handle email submitted
+  const onsubmitEmail = async (e) => {
+    try {
+      e.preventDefault()
+      const { data } = await axios.post(backendUrl + '/api/auth/reset-otp-send', { email })
+
+      if (data.success) {
+        toast.success(data.message)
+        setIsEmailSend(true)
+      }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
     <div className="bg-[url('/bg_img.png')] bg-cover bg-center min-h-screen relative">
       {/* Navbar Logo */}
@@ -48,7 +74,8 @@ const PasswordReset = () => {
       </div>
 
       <div className="flex items-center justify-center min-h-screen px-4">
-        <form className="bg-gray-900 text-white rounded-xl shadow-xl w-full max-w-md p-8">
+        {/* email fields */}
+        {!isEmailSend && <form onSubmit={onsubmitEmail} className="bg-gray-900 text-white rounded-xl shadow-xl w-full max-w-md p-8">
           {/* Heading */}
           <h2 className="text-center text-2xl font-semibold mb-4">
             Reset Password
@@ -77,80 +104,83 @@ const PasswordReset = () => {
           >
             Verify Email
           </button>
-        </form>
+        </form>}
 
         {/* otp fields */}
-        <form className="bg-gray-900 text-white rounded-xl shadow-xl w-full max-w-md p-8">
-          {/* Heading */}
-          <h2 className="text-center text-2xl font-semibold mb-4">
-            Enter Your 6-Digit OTP
-          </h2>
-          <p className="text-center text-gray-300 mb-6">
-            Please enter the OTP sent to your email address
-          </p>
-
-          {/* OTP Fields */}
-          <div className="flex justify-between mb-6">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <input
-                key={index}
-                type="text"
-                maxLength={1}
-                className="w-12 h-12 text-center text-gray-900 rounded-lg bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                ref={event => inputRef.current[index] = event}
-                onInput={(e) => handleInput(e, index)}
-                onKeyDown={(e) => handleKeyDown(e, index)}
-                onPaste={handlePaste}
-              />
-            ))}
-          </div>
-
-          {/* Submit Button */}
-          <button className="w-full bg-blue-600 hover:bg-blue-700 py-2 rounded-lg text-white font-semibold transition">
-            Submit
-          </button>
-        </form>
-
-
-
-        {/* new password */}
-        <div className="flex items-center justify-center min-h-screen px-4">
+        {!isOtpSubmitted && isEmailSend &&
           <form className="bg-gray-900 text-white rounded-xl shadow-xl w-full max-w-md p-8">
-
             {/* Heading */}
             <h2 className="text-center text-2xl font-semibold mb-4">
-              Enter New Password
+              Enter Your 6-Digit OTP
             </h2>
             <p className="text-center text-gray-300 mb-6">
-              Please enter  your new password
+              Please enter the OTP sent to your email address
             </p>
 
-            {/* new password Fields */}
-            <div className="flex items-center border rounded-lg px-3 py-2 bg-gray-50">
-              <img src={assets.lock_icon} alt="lock" className="w-5 h-5 mr-2" />
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter Your New Password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full bg-transparent outline-none text-gray-700"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="text-gray-600 ml-2"
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
+            {/* OTP Fields */}
+            <div className="flex justify-between mb-6">
+              {Array.from({ length: 6 }).map((_, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  maxLength={1}
+                  className="w-12 h-12 text-center text-gray-900 rounded-lg bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  ref={event => inputRef.current[index] = event}
+                  onInput={(e) => handleInput(e, index)}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                  onPaste={handlePaste}
+                />
+              ))}
             </div>
 
             {/* Submit Button */}
-            <button className="w-full bg-blue-600 hover:bg-blue-700 py-2 mt-6 rounded-lg text-white font-semibold transition">
+            <button className="w-full bg-blue-600 hover:bg-blue-700 py-2 rounded-lg text-white font-semibold transition">
               Submit
             </button>
           </form>
-        </div>
+        }
+
+
+        {/* new password */}
+        {isOtpSubmitted && isEmailSend &&
+          <div className="flex items-center justify-center min-h-screen px-4">
+            <form className="bg-gray-900 text-white rounded-xl shadow-xl w-full max-w-md p-8">
+
+              {/* Heading */}
+              <h2 className="text-center text-2xl font-semibold mb-4">
+                Enter New Password
+              </h2>
+              <p className="text-center text-gray-300 mb-6">
+                Please enter  your new password
+              </p>
+
+              {/* new password Fields */}
+              <div className="flex items-center border rounded-lg px-3 py-2 bg-gray-50">
+                <img src={assets.lock_icon} alt="lock" className="w-5 h-5 mr-2" />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter Your New Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full bg-transparent outline-none text-gray-700"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="text-gray-600 ml-2"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+
+              {/* Submit Button */}
+              <button className="w-full bg-blue-600 hover:bg-blue-700 py-2 mt-6 rounded-lg text-white font-semibold transition">
+                Submit
+              </button>
+            </form>
+          </div>
+        }
       </div>
     </div>
   )
